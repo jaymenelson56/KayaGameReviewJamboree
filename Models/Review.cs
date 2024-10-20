456000f52b4c97f57c04238a704ef8f6245e8bea
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2024-10-20 04:34:16 -0500

    added review model

---
+++ Models/Review.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace KayaGameReviewJamboree.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    public string Title { get; set ;}
+    public string Body { get; set; }
+    public int UserId { get; set;}
+    public int ReactionId { get; set; }
+}
