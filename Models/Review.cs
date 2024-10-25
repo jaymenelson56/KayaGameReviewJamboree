d25e3e27eeaaa0c4c672de81e1a4fd411e3e849d
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2024-10-24 21:43:51 -0500

    updated model

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace KayaGameReviewJamboree.Models;
@@ -5,8 +7,14 @@ namespace KayaGameReviewJamboree.Models;
 public class Review
 {
     public int Id { get; set; }
+    [Required]
     public string? Title { get; set ;}
+    [Required]
     public string? Body { get; set; }
+    [Required]
     public int UserId { get; set;}
+    [Required]
     public int ReactionId { get; set; }
+    [ForeignKey(nameof(UserId))]
+    public UserProfile UserProfile { get; set; }
 }
