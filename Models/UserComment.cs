0ca1258b124e017aff8cab273b7bff9a8f46000f
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2024-11-04 11:26:13 -0600

    modified models

--- Models/UserComment.cs
+++ Models/UserComment.cs
@@ -1,13 +1,19 @@
-using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KayaGameReviewJamboree.Models;
 
-public class Comment
+public class UserComment
 {
-    public int Id { get; set; }
+    
+    [Required]
     public string? Body { get; set; }
+    [Required]
     public int ReviewId { get; set; }
+    [Required]
     public int UserProfileId { get; set; }
+    [ForeignKey(nameof(ReviewId))]
     public Review? Review { get; set; }
+    [ForeignKey(nameof(UserProfileId))]
     public UserProfile? UserProfile { get; set;}
 }   
--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace KayaGameReviewJamboree.Models;
@@ -5,12 +7,19 @@ namespace KayaGameReviewJamboree.Models;
 public class UserProfile
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Address { get; set; }
+    [Required(ErrorMessage = "First name is required")]
+    public string? FirstName { get; set; }
+    [Required(ErrorMessage = "Last name is required")]
+    public string? LastName { get; set; }
+    [Required]
+    public string? Address { get; set; }
+    
+    [NotMapped]
+    public string? Email { get; set; }
+    [Required]
 
-    public string IdentityUserId { get; set; }
+    public string? IdentityUserId { get; set; }
 
-    public IdentityUser IdentityUser { get; set; }
+    public IdentityUser? IdentityUser { get; set; }
 
 }
