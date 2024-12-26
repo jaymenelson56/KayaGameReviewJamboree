278b206964cd659337d07085234016857f164fdb
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2024-12-26 14:34:51 -0600

    added to reviewmanager and review.jsx

--- client/src/managers/reviewManager.js
+++ client/src/managers/reviewManager.js
@@ -3,3 +3,7 @@ const _apiUrl = "api/Review";
 export const getReviewList = () => {
     return fetch(_apiUrl + "/list").then((res) => res.json());
 };
+
+export const getReview = (reviewId) => {
+    return fetch(_apiUrl + `/${reviewId}`).then((res) => res.json());
+}
